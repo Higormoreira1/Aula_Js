4e65b7a2bdb2a75188a1038261e1f6ad33449f32
Higor Moreira <higorantonio26@gmail.com>
2021-01-28 19:45:44 -0200

    Html+Css

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let nome ="Intro ao Javascript/Html";
+//alert("Bem-vindo a " + nome);/*Comando onde um Pop-up surge na página apresentando o texto
+//                          que foi adicionado dentro do comando "alert"*/
+console.log("Bem-vindo a " + nome);//Serve para ver o que o Js está realizando direto no navegador através do Cntrl+Shift+I
+let frase="Bahia é o maior clube do Norte/Nordeste";
+console.log(frase.replace("Bahia","Sport"));//Troca a palavra atribuida inicialmente, pela palavra declarada ao lado da inicial dentro do replace"   
+alert(frase.replace("Bahia","Sport"));
